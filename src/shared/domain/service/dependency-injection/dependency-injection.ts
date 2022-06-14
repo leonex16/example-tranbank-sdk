1bc43cc406feb9443aff1fa9be4474660eadd247
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-06-14 00:46:50 -0400

    Add dependency injection decorators

---
+++ src/shared/domain/service/dependency-injection/dependency-injection.ts
@@ -0,0 +1,35 @@
+import { DependencyKey, Injection } from './types';
+
+export class Container {
+  private static _registry: Map<string, any> = new Map();
+
+  static register ( key: DependencyKey, instance: any ) {
+    if ( Container._registry.has( key ) === false ) Container._registry.set( key, instance );;
+    return this;
+  }
+
+  static getInstance ( key: DependencyKey ) {
+    return Container._registry.get( key );
+  }
+}
+
+export function Inject ( key: DependencyKey ) {
+  return function ( target: any, propertyKey: string, parameterPosition: number ) {
+    const injection: Injection = { index: parameterPosition, key };
+    const existingInjections: Injection[] = target.injections ?? [];
+    target.injections = [ ...existingInjections, injection ];
+  };
+}
+
+export function InjectionTarget () {
+  return function <T extends { new ( ...args: any[] ): object }>( constructor: T ) {
+    return class extends constructor {
+      constructor ( ..._: any[] ) {
+        const injections = ( constructor as any ).injections as Injection[];
+        const injectedArgs: any[] = injections.map( ( { key } ) => Container.getInstance( key ) );
+
+        super( ...injectedArgs );
+      }
+    };
+  };
+}
---
+++ src/shared/domain/service/dependency-injection/types.d.ts
@@ -0,0 +1,3 @@
+export type DependencyKey = 'PaymentMethod' | 'Server';
+
+export type Injection = { index: number, key: DependencyKey };
