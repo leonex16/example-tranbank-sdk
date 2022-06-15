07cb78f401cd58f320668f4498b9fc7fbf5ef946
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-06-15 15:01:17 -0400

    (application) Add test to payment method

---
+++ test/modules/payment-method/application/__mocks__/payment-method-spy.ts
@@ -0,0 +1,31 @@
+import { PaymentMethod } from '../../../../../src/modules/payment-method/domain/payment-method';
+
+export class PaymentMethodSpy implements PaymentMethod {
+  public methodCalledCounter = 0;
+
+  get cardInfo(): { type: string; number: string; } {
+    throw new Error('Method not implemented.');
+  }
+
+  get keysToTransaction(): any {
+    throw new Error('Method not implemented.');
+  }
+
+  async getUrlToAdd() {
+    this.methodCalledCounter++;
+
+    return { url: '', token: '' }
+  }
+
+  async confirm() {
+    this.methodCalledCounter++;
+  }
+
+  async delete() {
+    this.methodCalledCounter++;
+  }
+
+  clearSpy() {
+    this.methodCalledCounter = 0;
+  }
+}
