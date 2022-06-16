30f91be2f99bd761fb87170234e891c46fb38393
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-06-16 19:08:36 -0400

    (domain) Add deferred transaction contract

---
+++ src/modules/deferred-transaction/domain/deferred-transaction.ts
@@ -0,0 +1,6 @@
+export interface DeferredTransaction {
+  autorize( username: string, tbkUser: string, purchaseOrder: string, detail: unknown[] ): Promise<any>;
+  getStatus( purchaseOrder: string ): Promise<any>;
+  reverse( mainPurchaseOrder: string, childPurchaseOrder: string, amount: number ): Promise<any>;
+  capture( childPurchaseOrder: string, amountToCapture: number, authorizationCode: string ): Promise<any>;
+}
