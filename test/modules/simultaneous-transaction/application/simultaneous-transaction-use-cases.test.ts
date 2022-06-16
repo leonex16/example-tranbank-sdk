3265220c8148ed8eddf935abac7c1d0a90a01147
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-06-16 16:31:39 -0400

    (application) add test for simultaneous transaction use cases

---
+++ test/modules/simultaneous-transaction/application/simultaneous-transaction-use-cases.test.ts
@@ -0,0 +1,70 @@
+import { expect, test } from '@playwright/test';
+
+import { SimultaneousTransactionAutorizator } from '../../../../dist/src/modules/simultaneous-transaction/application/simultaneous-transaction-autorizator';
+import { SimultaneousTransactionReversor } from '../../../../dist/src/modules/simultaneous-transaction/application/simultaneous-transaction-reversor';
+import { SimultaneousTransactionSpy } from './__mocks__/simultaneous-transaction-spy';
+import { SimultaneousTransactionStatus } from '../../../../dist/src/modules/simultaneous-transaction/application/simultaneous-transaction-status';
+import { registerDependencies } from '../../../../dist/src/config/register-dependencies';
+
+const username = 'U-92424';
+const tbkUser = 'a0ece32c-8b50-45e8-9fbd-0c76fbea3dbd';
+const purchaseOrder = `PO-${ new Date().getTime() }-`;
+const detail = [ { amount: Math.floor( Math.random() * ( 10_000 - 1_000 ) + 1_000 ) } ];
+
+test.describe( 'Application Simultaneous Transaction', () => {
+  test.describe( 'SimultaneousTransactionAutorizator', () => {
+    test( 'should called invoke method', async () => {
+      const simultaneousTransactionSpy = new SimultaneousTransactionSpy();
+      const simultaneousTransactionAutorizator = new SimultaneousTransactionAutorizator( simultaneousTransactionSpy );
+      await simultaneousTransactionAutorizator.invoke( 'USERNAME', 'TBK_USER', 'PO-1234', [] );
+
+      expect( simultaneousTransactionSpy.methodCalledCounter ).toBe( 1 );
+    } );
+
+    test( 'should inyject automatically its dependecy', async () => {
+      registerDependencies();
+      const simultaneousTransactionAutorizator = new SimultaneousTransactionAutorizator();
+
+      await expect( simultaneousTransactionAutorizator.invoke( username, tbkUser, purchaseOrder, detail ) ).resolves.toBeTruthy();
+    } );
+  } );
+
+  test.describe( 'SimultaneousTransactionStatus', () => {
+    test( 'should called invoke method', async () => {
+      const simultaneousTransactionSpy = new SimultaneousTransactionSpy();
+      const simultaneousTransactionStatus = new SimultaneousTransactionStatus( simultaneousTransactionSpy );
+      await simultaneousTransactionStatus.invoke( 'PO-1234' );
+
+      expect( simultaneousTransactionSpy.methodCalledCounter ).toBe( 1 );
+    } );
+
+    test( 'should inyject automatically its dependecy', async () => {
+      registerDependencies();
+      const simultaneousTransactionAutorizator = new SimultaneousTransactionAutorizator();
+      const simultaneousTransactionStatus = new SimultaneousTransactionStatus();
+      const transaction = await simultaneousTransactionAutorizator.invoke( username, tbkUser, purchaseOrder, detail );
+
+      await expect( simultaneousTransactionStatus.invoke( transaction.buyOrder ) ).resolves.toBeTruthy();
+    } );
+  } );
+
+  test.describe( 'SimultaneousTransactionReversor', () => {
+    test( 'should called invoke method', async () => {
+      const simultaneousTransactionSpy = new SimultaneousTransactionSpy();
+      const simultaneousTransactionReversor = new SimultaneousTransactionReversor( simultaneousTransactionSpy );
+      await simultaneousTransactionReversor.invoke( 'PO-XXX-0', 'PO-XXX-1', 5000 );
+
+      expect( simultaneousTransactionSpy.methodCalledCounter ).toBe( 1 );
+    } );
+
+    test( 'should inyject automatically its dependecy', async () => {
+      registerDependencies();
+      const simultaneousTransactionAutorizator = new SimultaneousTransactionAutorizator();
+      const simultaneousTransactionReversor = new SimultaneousTransactionReversor();
+      const transaction = await simultaneousTransactionAutorizator.invoke( username, tbkUser, purchaseOrder, detail );
+      const item = transaction.details[ 0 ];
+
+      await expect( simultaneousTransactionReversor.invoke( transaction.buyOrder, item.buyOrder, item.amount ) ).resolves.toBeTruthy();
+    } );
+  } );
+} );
