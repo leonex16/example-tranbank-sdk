c85ccf0b5697fc22bcc96a71b6d6258c56ecb885
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-06-16 19:14:01 -0400

    (infrastructure) Add deferred transaction one click implementation

---
+++ src/modules/deferred-transaction/infrastructure/transbank/one-click/types.d.ts
@@ -0,0 +1,38 @@
+export interface CardDetail {
+  card_number: string;
+}
+
+export interface Detail {
+  amount: number;
+  status: string;
+  authorization_code: string;
+  payment_type_code: string;
+  response_code: number;
+  installments_number: number;
+  commerce_code: string;
+  buy_order: string;
+}
+
+export interface TranbankTransaction {
+  details: Detail[];
+  buy_order: string;
+  card_detail: CardDetail;
+  accounting_date: string;
+  transaction_date: Date;
+}
+
+export interface TransbankRefound {
+  type: 'REVERSED'
+}
+
+export interface TransactionItem {
+  amount: number,
+  installments?: number
+}
+
+export interface TranbankCapture {
+  authorization_code: string;
+  authorization_date: Date;
+  captured_amount: number;
+  response_code: number;
+}
