027eb366de4f5f337f29e71f08afb5b2154d07d1
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-06-16 00:14:53 -0400

    (application) add simultaneous transaction status use case

---
+++ src/modules/simultaneous-transaction/application/simultaneous-transaction-status.ts
@@ -0,0 +1,15 @@
+import { SimultaneousTransaction } from "#src/modules/simultaneous-transaction/domain/simultaneous-transaction";
+
+export class SimultaneousTransactionStatus {
+  constructor(
+    private _transaction?: SimultaneousTransaction
+  ) { }
+
+  async invoke(purchaseOrder: string): Promise<any> {
+    if ( !this._transaction ) throw new Error('TransactionAutorizator needs a transaction instance');
+    if ( !purchaseOrder ) throw new Error('PurchaseOrder is required');
+    if ( purchaseOrder.slice(0, 3) !== 'PO-' ) throw new Error('PurchaseOrder must start with "PO-"');
+
+    return this._transaction.getStatus(purchaseOrder)
+  }
+}
