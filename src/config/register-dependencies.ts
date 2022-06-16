83111f7c3d5d3d5dc019315947f032afdf29504f
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-06-16 15:55:39 -0400

    Register simultaneous transaction dependency

--- src/config/register-dependencies.ts
+++ src/config/register-dependencies.ts
@@ -1,9 +1,11 @@
 import { Container } from '#src/shared/domain/service/dependency-injection/index';
 import { NodeServer } from '#src/modules/server/infrastructure/vanilla/server';
 import { TransbankOneClickPaymentMethod } from '#src/modules/payment-method/insfrastructure/transbank/one-click/payment-method';
+import { TransbankOneClickTransaction } from '#src/modules/simultaneous-transaction/infrastructure/transbank/one-click/simultaneous-transaction';
 
 export const registerDependencies = () => {
   Container
     .register( 'PaymentMethod', new TransbankOneClickPaymentMethod() )
+    .register( 'SimultaneousTransaction', new TransbankOneClickTransaction() )
     .register( 'Server', new NodeServer() );
 };
