a24bb300d58eb891808dec72622f2b883364c852
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-06-16 23:40:30 -0400

    Fix path

--- test/shared/get-url-to-card-register.ts
+++ test/shared/get-url-to-card-register.ts
@@ -1,5 +1,5 @@
 import { PaymentMethod } from '../../src/modules/simultaneous/payment-method/domain/payment-method';
-import { PaymentMethodCreator } from '../../dist/src/modules/payment-method/application/payment-method-creator';
+import { PaymentMethodCreator } from '../../dist/src/modules/simultaneous/payment-method/application/payment-method-creator';
 
 export const getUrlToCardRegister = async ( paymentMethodImplementation: PaymentMethod ) => {
   const username = `U-${ Math.floor( Math.random() * 1000000 ) }`;
